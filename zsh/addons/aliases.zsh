ada1bee64c73a6c143dff89f94bb5b75fcc4c37e
Jack Stratton <nr.phroa@gmail.com>
2015-01-10 10:35:31 -0800

    Add several aliases

---
+++ zsh/addons/aliases.zsh
@@ -0,0 +1,17 @@
+alias ae="vim $HOME/dotfiles/zsh/addons/aliases.zsh"
+alias ar=". $HOME/dotfiles/zsh/addons/aliases.zsh"
+
+alias df='df -h'
+alias du='du -h -d 2'
+alias ls='ls -Gh --color=auto'
+alias less='less --color=auto'
+alias grep='grep --color=auto'
+
+alias l='ls -alFh'
+
+alias ga='git add -A'
+alias gc='git commit'
+alias gi='vim .gitignore'
+alias gl='git log'
+alias gp='git push'
+alias gs='git status'
